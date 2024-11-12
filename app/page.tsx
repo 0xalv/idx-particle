c588b1cde2bd1760ff33a9fd614879fe61ba4e02
alv <alv.web3@gmail.com>
2024-11-12 19:19:36 +0100

    chore: minor vercel config update for proxying api requests (now moving the endpoint call to client side)

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
+// // pages/index.js
+
+// import ProductTable from "@/components/dashboard/ProductTable";
+
+// const Page = async () => {
+//   // const response = await fetch(
+//   //   "http://ec2-54-174-164-111.compute-1.amazonaws.com/api/tokens"
+//   // );
+//   const response = await fetch("/api/tokens");
+//   const products = await response.json();
+
+//   return (
+//     <div>
+//       <ProductTable products={products} />
+//     </div>
+//   );
+// };
+
+// export default Page;
+
 // pages/index.js
 
 import ProductTable from "@/components/dashboard/ProductTable";
 
-const Page = async () => {
-  // const response = await fetch(
-  //   "http://ec2-54-174-164-111.compute-1.amazonaws.com/api/tokens"
-  // );
-  const response = await fetch("/api/tokens");
-  const products = await response.json();
-
+const Page = () => {
   return (
     <div>
-      <ProductTable products={products} />
+      <ProductTable />
     </div>
   );
 };
--- src/components/dashboard/ProductTable.tsx
+++ src/components/dashboard/ProductTable.tsx
@@ -1,5 +1,6 @@
 "use client"; // Marks this component as a Client Component
 
+import { useState, useEffect } from "react";
 import tokenLogos from "@/data/tokenLogos.json"; // Assuming you still need this for logos
 import Link from "next/link";
 
@@ -17,7 +18,23 @@ interface Product {
   tvl: string;
 }
 
-const ProductTable = ({ products }: { products: Product[] }) => {
+const ProductTable = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("/api/tokens");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="px-2 pt-16 sm:px-4 md:px-8 md:pt-20 lg:px-12">
       <div className="flex flex-col items-center space-y-10 md:space-y-12">
